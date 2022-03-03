2f10a058479611fd89e2f854debb7428bad1bbf3
vaishnavithakare2807 <vaishnavithaakare@gmail.com>
2022-03-03 09:40:29 +0530

    .

---
+++ app.js
@@ -0,0 +1,22 @@
+let array = ["red", "orange", "pink", "black", "white", "blue"];
+console.log(array);
+let one = array[0];
+console.log(one);
+let two = array[1];
+console.log(two)
+
+
+
+
+
+
+
+
+let [first, second, third, fourth, five, six] = array;
+console.log(` the first color is ${first},second color is ${second}`);
+
+
+console.log(array.length);
+let [firstElement, , , , , lastElement] = array;
+console.log(`last element in the array is ${lastElement}`)
+
